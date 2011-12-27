5bf7ec88ec436d37e794ccc8f3aea258cb237c2c
Knut Henriksen <tubaknut@gmail.com>
2011-12-27 11:04:45 +0100

    Added length control for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10} 
   validates :image_url, allow_blank: true, 
       format: {
         with: %r{\.(gif|png|jpg)$}i, message: 'Must be a valid format.'
